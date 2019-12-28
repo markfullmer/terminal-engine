68b8693ae5addcdf4950cd57003879a699b017c6
Mark Fullmer <mfullmer@gmail.com>
2019-12-28 13:38:19 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: './src/index.ts',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			deepmerge$: path.resolve(__dirname, 'node_modules/deepmerge/dist/umd.js'),
+		},
+		extensions: [ '.tsx', '.ts', '.js' ]
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		library: 'terminal-engine',
+		libraryTarget: 'umd',
+		umdNamedDefine: true
+	}
+};
